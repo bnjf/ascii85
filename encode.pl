851b92fef33555e36ce6361d66b94795b8421547
Brad <github@bnjf.id.au>
2020-01-29 17:32:33 +1100

    fix undef errors

--- encode.pl
+++ encode.pl
@@ -8,20 +8,24 @@ use warnings;
 my ($in, $pos, $width) = (undef, 2, 72);
 print "<~";
 while (read(STDIN, $in, 4) > 0) {
-    my (@a, @buf, $x, $i);
+    my @buf;
 
-    @a = unpack("C4", $in);
-
-    $x = $a[0] << 24 | $a[1] << 16 | $a[2] << 8 | $a[3];
-    if (scalar(@a) == 4 && $x == 0) {
+    if ($in eq "\0\0\0\0") {
         $buf[0] = "z";                                      # '!!!!' -> 'z'
     } else {
+        my (@a, $x, $i);
+
+        @a = unpack("C*", $in);
+        @a = map { $a[$_] || 0 } 0..3;
+        $x = $a[0] << 24 | $a[1] << 16 | $a[2] << 8 | $a[3];
+
         for $i (1 .. 5) {
             unshift @buf, chr(($x % 85) + 33);
             $x /= 85;
         }
-        splice @buf, scalar(@a) + 1;                        # trim @buf
+        splice @buf, length($in) + 1;                       # trim @buf
     }
+
     if (scalar(@buf) + $pos > $width) {                     # should we wrap?
         $pos = ($pos + scalar(@buf)) % ($width + 1);
         splice @buf, scalar(@buf) - $pos, 0, "\n";          # put a newline in
